092a879e7d3ea282f4960cdbfea4e56cd17ac597
Tayyab Nisar <tayyab.nisar.7719@gmail.com>
2025-01-06 12:52:22 +0500

    feat: configure the webpack with react.js, material-ui
    Configuration done with webpack. Now project is working using React.js, Material-ui

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box, Button, Typography } from '@mui/material';
+
+const App = () => {
+    return (
+        <Box>
+            <Button>Click Me</Button>
+            <Typography variant='h1'>Hello World</Typography>
+        </Box>
+    );
+};
+
+export default App;
